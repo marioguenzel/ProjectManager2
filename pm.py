2297e326d020a29aea04629ceee55a4a8b18e85f
Mario <mario.guenzel@tu-dortmund.de>
2025-02-08 11:09:27 +0100

    making progress with keybindings and basic functionality

--- pm.py
+++ pm.py
@@ -11,7 +11,57 @@ from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.widgets import TextArea
 
+HELP_MESSAGE = """
+    ### All modes (OPEN and GROUP) ###
+    # TUI handling
+    open <PROJECT>  -> open a project and show its resources
+
+    group <CATEGORY>  -> open the group view with category (use 'group *' to show all projects without grouping)
+    filter <CATEGORY> <CONTEXT>  -> only show projects with that context
+
+    note <PROJECT>   -> open markdown note
+    qnote <PROJECT>  -> modify quicknote
+
+    context-note <CATEGORY> <CONTEXT>  -> open markdown note 
+    context-note <CATEGORY> <CONTEXT>  -> modify quicknote 
+
+    # Modifications
+    + create <PROJECT>      -> create a new project
+    archive <PROJECT>     -> archive a project
+    unarchive <PROJECT>   -> unarchive a project
+
+    link <PROJECT> <CATEGORY> <CONTEXT>    -> Create a new link
+    unlink <PROJECT> <CATEGORY> <CONTEXT>  -> Remove a link
+
+    context-create <CATEGORY> <CONTEXT>   -> create a new context
+    context-archive <CATEGORY> <CONTEXT>  -> archive a context
+    context-unarchive <CATEGORY> <CONTEXT>  -> unarchive a context
+
+    + code    -> Open vscode of the folder to make modifications manually
+    + reload  -> Make sure to reload when making manual modifications
+    + dump  -> Dump changes to file (Will be removed in later version)
+
+    backup  -> push changes to git (git -A commit; push)
+
+    ### In OPEN-mode only ###
+    resource <RESOURCE> <ACTION>  -> do action for resource
+    resource-create <RESOURCE> <TYPE> <source>  -> create a resource of TYPE with source
+    resource-info <RESOURCE>      -> show infor of that resource 
+
+    ### ACTIONS ###
+    - SVN: code, clone
+    - GIT: code, clone
+    - LINK: open
+
+    ### keys ###
+    + ctrl-q  -> Quit
+    + f1      -> Show help
+    f2      -> Show categories
+    esc     -> scroll to top
+"""
+
 class Data:
+    """Data loading, dumping and modification"""
     def __init__(self, LOCATION: Path):
         self.LOCATION = LOCATION
         self.Projects = dict()
@@ -55,19 +105,36 @@ class Data:
 
 
 class TUIManager:
+    """Manages how to show the data."""
     def __init__(self, DATA: Data):
         self.CONTENT = DATA
         self.mode = 'group'  # Can be show or group 
-        self.mode_content = ['*']  # List of what is feeded into the mode
+        self.mode_content = '*'  # Category or Project
         self.filter = []
         self.line_start = 0  # show from beginning by default
         # self.line_length = 10  # show 10 lines by default
+        self.help_message_visible = False
+        self.help_message_line = 0
     
     def return_top_text(self):
-        return '\n'.join(['- ' + str(proj) for proj in self.CONTENT.Projects])
+        if self.help_message_visible:
+            text_rows = HELP_MESSAGE.splitlines()
+            return '\n'.join(text_rows[self.help_message_line:])
+        else:
+            text_rows = ['- ' + str(proj) for proj in self.CONTENT.Projects]
+            return '\n'.join(text_rows[self.line_start:])
 
-def CommandParser(data: Data, tuimanager: TUIManager, *args):
-    pass
+def CommandParser(data: Data, tuimanager: TUIManager, args):
+    if args[0] == 'code':
+        os.system(f"code '{data.LOCATION}'")
+    elif args[0] == 'reload':
+        data.load()
+    elif args[0] == 'dump':
+        data.dump()
+    elif args[0] == 'create':
+        data.add_project(args[1])
+    else:
+        raise ValueError(f"Unknown Arguments {args}")
 
 
 def main():
@@ -83,7 +150,7 @@ def main():
     data.load()
 
     # Start the Manager
-    man = TUIManager(Data(LOCATION))
+    man = TUIManager(data)
     
 
     ####
@@ -98,14 +165,46 @@ def main():
     def exit_app(event):
         event.app.exit()
 
+    # Show help
+    @kb.add('f1')
+    def exit_app(event):
+        man.help_message_visible = not man.help_message_visible
+        output_text.text = man.return_top_text()
+
+
     # Event when Enter is pressed
     @kb.add('enter')
     def handle_enter(event):
         command = command_input.text  # get text
-        # CommandParser(command.split())  # Parse the command
+        CommandParser(data, man, command.split())  # Parse the command
         output_text.text = man.return_top_text()
         command_input.text = ''  # Clear the input area
         # raise ValueError('This is a test')  # TODO Just raise value error if options do not work
+    
+    # Scrolling text functionality
+    @kb.add('down')
+    def handle_down(event):
+        if man.help_message_visible:
+            man.help_message_line += 1
+        else:
+            man.line_start +=1
+        output_text.text = man.return_top_text()
+    
+    @kb.add('up')
+    def handle_up(event):
+        if man.help_message_visible:
+            man.help_message_line = max(man.help_message_line- 1,0)
+        else:
+            man.line_start = max(man.line_start-1,0)
+        output_text.text = man.return_top_text()
+    
+    @kb.add('escape')  # jumping to 0
+    def handle_escape(event):
+        if man.help_message_visible:
+            man.help_message_line = 0
+        else:
+            man.line_start = 0
+        output_text.text = man.return_top_text()
 
 
     # Top section - static text and output display
@@ -159,6 +258,8 @@ def main():
         full_screen=True
     )
 
+    # Load once
+    output_text.text = man.return_top_text()
     application.run()
 
     # breakpoint()
